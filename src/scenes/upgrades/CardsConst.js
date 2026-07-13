41315003fd4c782ca875b7db4ffd7638e2d73296
Kilin0lk <maximkushp@gmail.com>
2026-07-13 23:24:35 +0400

    Доработал карты

--- src/scenes/upgrades/CardsConst.js
+++ src/scenes/upgrades/CardsConst.js
@@ -1,47 +1,11 @@
 export const CARDS = [
     {
-        id: 'maxHpMultiplier',
-        imageKey: 'HpMultiplierCard',
-        weight: 4,
-        apply(player) {
-            player.maxHp *= 2;
-            player.storage.save('maxHpMultiplier', player.maxHp);
-        }
-    },
-    {
         id: 'maxTenHp',
         imageKey: 'CardTenMaxHp',
         weight: 15,
         apply(player) {
             player.maxHp += 10;
             player.storage.save('maxTenHp', player.maxHp);
-        }
-    },
-    {
-        id: 'maxFifteenHp',
-        imageKey: 'CardFifteenMaxHp',
-        weight: 8,
-        apply(player) {
-            player.maxHp += 15;
-            player.storage.save('maxFifteenHp', player.maxHp);
-        }
-    },
-        {
-        id: 'maxFiftyHp',
-        imageKey: 'CardFiftyMaxHp',
-        weight: 7,
-        apply(player) {
-            player.maxHp += 50;
-            player.storage.save('maxFiftyHp', player.maxHp);
-        }
-    },
-    {
-        id: 'coinMultiplier',
-        imageKey: 'CardDoubleCoins',
-        weight: 3,
-        apply(player) {
-            player.coinMultiplier *= 2;
-            player.storage.save('coinMultiplier', player.coinMultiplier);
         }
     },
     {
@@ -54,39 +18,30 @@ export const CARDS = [
         }
     },
     {
-        id: 'coinForChestFifteen',
-        imageKey: 'CardFifteenMoney',
-        weight: 8,
-        apply(player) {
-            player.baseCoinsIncrease += 15;
-            player.storage.save('coinForChestFifteen', player.baseCoinsIncrease);
-        }
-    },
-    {
-        id: 'coinForChestFifty',
-        imageKey: 'CardFiftyMoney',
-        weight: 7,
-        apply(player) {
-            player.baseCoinsIncrease += 50;
-            player.storage.save('coinForChestFifty', player.baseCoinsIncrease);
-        }
-    },
-    {
-        id: 'damageMultiplier',
-        imageKey: 'CardDoubleDamage',
-        weight: 3,
-        apply(player) {
-            player.damageMultiplier *= 2;
-            player.storage.save('damageMultiplier', player.damageMultiplier);
-        }
-    },
-    {
         id: 'damageFive',
         imageKey: 'CardFiveDamage',
         weight: 15,
         apply(player) {
             player.baseDamageIncrease += 5;
             player.storage.save('CardFiveDamage', player.baseDamageIncrease);
+        }
+    },
+    {
+        id: 'maxFifteenHp',
+        imageKey: 'CardFifteenMaxHp',
+        weight: 8,
+        apply(player) {
+            player.maxHp += 15;
+            player.storage.save('maxFifteenHp', player.maxHp);
+        }
+    },
+    {
+        id: 'coinForChestFifteen',
+        imageKey: 'CardFifteenMoney',
+        weight: 8,
+        apply(player) {
+            player.baseCoinsIncrease += 15;
+            player.storage.save('coinForChestFifteen', player.baseCoinsIncrease);
         }
     },
     {
@@ -99,12 +54,57 @@ export const CARDS = [
         }
     },
     {
+        id: 'maxFiftyHp',
+        imageKey: 'CardFiftyMaxHp',
+        weight: 7,
+        apply(player) {
+            player.maxHp += 50;
+            player.storage.save('maxFiftyHp', player.maxHp);
+        }
+    },
+    {
+        id: 'coinForChestFifty',
+        imageKey: 'CardFiftyMoney',
+        weight: 7,
+        apply(player) {
+            player.baseCoinsIncrease += 50;
+            player.storage.save('coinForChestFifty', player.baseCoinsIncrease);
+        }
+    },
+    {
         id: 'damageFifteen',
         imageKey: 'CardFifteenDamage',
         weight: 7,
         apply(player) {
             player.baseDamageIncrease += 15;
             player.storage.save('CardFifteenDamage', player.baseDamageIncrease);
+        }
+    },
+    {
+        id: 'maxHpMultiplier',
+        imageKey: 'HpMultiplierCard',
+        weight: 4,
+        apply(player) {
+            player.maxHp *= 2;
+            player.storage.save('maxHpMultiplier', player.maxHp);
+        }
+    },
+    {
+        id: 'coinMultiplier',
+        imageKey: 'CardDoubleCoins',
+        weight: 3,
+        apply(player) {
+            player.coinMultiplier *= 2;
+            player.storage.save('coinMultiplier', player.coinMultiplier);
+        }
+    },
+    {
+        id: 'damageMultiplier',
+        imageKey: 'CardDoubleDamage',
+        weight: 3,
+        apply(player) {
+            player.damageMultiplier *= 2;
+            player.storage.save('damageMultiplier', player.damageMultiplier);
         }
     }
 ];
